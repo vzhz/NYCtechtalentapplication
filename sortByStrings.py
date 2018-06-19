f0e0c38bd1d9aef0202d19b11966162c8a156edf
vzhz <vehanus@gmail.com>
2018-06-18 22:57:07 -0400

    add files with problem statements

---
+++ sortByStrings.py
@@ -0,0 +1,6 @@
+######
+# Question 1 -- sortByStrings(s,t): Sort the letters in the string s by the order they occur 
+# in the string t. You can assume t will not have repetitive characters. For s = "weather" 
+# and t = "therapyw", the output should be sortByString(s, t) = "theeraw". For s = "good" 
+# and t = "odg", the output should be sortByString(s, t) = "oodg".
+######
